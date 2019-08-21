c402f70d8d519b03a499bc5a303eece4c53a5a8e
Paul Mckee <paul.mckee.aus@gmail.com>
2019-08-22 00:32:26 +1000

    Add app3 with Typescript

---
+++ app3/src/main.ts
@@ -0,0 +1,28 @@
+// import './set-public-path';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import singleSpaVue from 'single-spa-vue';
+
+
+Vue.config.productionTip = false;
+
+// new Vue({
+//   router,
+//   store,
+//   render: (h) => h(App),
+// }).$mount('#app');
+
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render: (h: any) => h(App),
+    router,
+    store,
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
